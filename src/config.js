ef2f2db1f65af369ead2d9805789b8facde86ef0
Karm Patel <contact.karmpatel@gmail.com>
2025-01-03 19:00:07 +0530

    Remove Some Files

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCiS09kt4dUPNsJw751Tb33t4JGTKC4YNY",
+    authDomain: "todo-site-befd5.firebaseapp.com",
+    projectId: "todo-site-befd5",
+    storageBucket: "todo-site-befd5.appspot.com",
+    messagingSenderId: "674064526188",
+    appId: "1:674064526188:web:825243ef95ef46a3b22149"
 };
 
 export default firebaseConfig;
